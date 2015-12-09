a1fa88683b6ccaf9c9fd0a911cb90849309c8eaf
Steve Harris <doc@steveharris.me>
2015-12-09 23:03:24 +0000

    added surgical time

--- bld_theatre.R
+++ bld_theatre.R
@@ -56,14 +56,20 @@ tdt.t1 <- rdt.t1[, .(
         ifelse(grepl("schedul*", Operation.Priority, ignore.case=T, perl=T), "scheduled",
         ifelse(grepl("urgent*", Operation.Priority, ignore.case=T, perl=T), "urgent",
         ifelse(grepl("immed*", Operation.Priority, ignore.case=T, perl=T), "emergent",NA)))),
-    anaesthesia.delay = difftime(
+    anaesthesia.time = difftime(
             as.POSIXct(Procedure_start_time, format="%d/%m/%Y %H:%M", tz="GMT"),
             as.POSIXct(Anaesthetic_start_time, format="%d/%m/%Y %H:%M", tz="GMT"),
+            units="mins"),
+    surgical.time = difftime(
+            as.POSIXct(Procedure_end_time, format="%d/%m/%Y %H:%M", tz="GMT"),
+            as.POSIXct(Procedure_start_time, format="%d/%m/%Y %H:%M", tz="GMT"),
             units="mins")
     )
     ]
 # truncate anaesthesia delay at 2 hours
-tdt.t1[, anaesthesia.delay:=ifelse(anaesthesia.delay>120 | anaesthesia.delay < 0,NA,anaesthesia.delay)]
+tdt.t1[, anaesthesia.time:=ifelse(anaesthesia.time>120 | anaesthesia.time < 0,NA,anaesthesia.time)]
+# truncate surgical time at 4 hours
+tdt.t1[, surgical.time:=ifelse(surgical.time>240 | surgical.time < 0,NA,surgical.time)]
 # Convert all fields to UTF-8, lower case, and truncate
 tdt.t1[, MRN  := iconv(MRN, 'WINDOWS-1252', 'UTF-8')] # convert windows char codes
 tdt.t1[, MRN  :=substr(tolower(MRN),1,10)]
@@ -72,6 +78,8 @@ tdt.t1[, namelast  :=strsplit(namefull, ',')[[1]][1], by=id.t1]
 tdt.t1[, namefirst :=strsplit(namefull, ',')[[1]][2], by=id.t1]
 tdt.t1[, namefull := NULL]
 tdt.t1[, id.t2 := NA]
+describe(tdt.t1$anaesthesia.time)
+describe(tdt.t1$surgical.time)
 str(tdt.t1)
 
 
@@ -100,15 +108,22 @@ tdt.t2 <- rdt.t2[, .(
         ifelse(grepl("schedul*", ncepod, ignore.case=T, perl=T), "scheduled",
         ifelse(grepl("urgent*", ncepod, ignore.case=T, perl=T), "urgent",
         ifelse(grepl("emerg*", ncepod, ignore.case=T, perl=T), "emergent",NA)))),
-    anaesthesia.delay = difftime(
+    anaesthesia.time = difftime(
         parse_date_time(time_actual_procedure_start, "HM"),
         parse_date_time(time_anesthesia_start, "HM"),
+        units="mins") ,
+    surgical.time = difftime(
+        parse_date_time(time_actual_procedure_end, "HM"),
+        parse_date_time(time_actual_procedure_start, "HM"),
         units="mins") 
     ) ]
 # where crosses midnight then add 60*24 back
-tdt.t2[, anaesthesia.delay:=ifelse(anaesthesia.delay<0,anaesthesia.delay+60*24,anaesthesia.delay)]
+tdt.t2[, anaesthesia.time:=ifelse(anaesthesia.time<0,anaesthesia.time+60*24,anaesthesia.time)]
+tdt.t2[, surgical.time:=ifelse(surgical.time<0,surgical.time+60*24,surgical.time)]
 # truncate anaesthesia delay at 2 hours
-tdt.t2[, anaesthesia.delay:=ifelse(anaesthesia.delay>120,NA,anaesthesia.delay)]
+tdt.t2[, anaesthesia.time:=ifelse(anaesthesia.time>120,NA,anaesthesia.time)]
+# truncate surgical time at 2 hours
+tdt.t2[, surgical.time:=ifelse(surgical.time>240,NA,surgical.time)]
 # Convert all fields to UTF-8, lower case, and truncate
 tdt.t2[, MRN  := iconv(MRN, 'WINDOWS-1252', 'UTF-8')] # convert windows char codes
 tdt.t2[, MRN  :=substr(tolower(MRN),1,10)]
@@ -119,11 +134,16 @@ tdt.t2[, namefirst :=str_trim(strsplit(namefull, ',')[[1]][2]), by=id.t2]
 tdt.t2[, namefull := NULL]
 tdt.t2[, id.t1 := NA]
 
+describe(tdt.t2$anaesthesia.time)
+describe(tdt.t2$surgical.time)
 str(tdt.t2)
 
 tdt.t <- rbind(tdt.t2, tdt.t1)
 tdt.t[, id.t := .I]
+str(tdt.t)
+names(tdt.t)
+setcolorder(tdt.t, 
+    c("id.t", "id.t1", "id.t2", names(tdt.t)[c(-1,-10,-11)]))
 tdt.t
-
 
 save(tdt.t, rdt.t1, rdt.t2, file='../data/theatre.RData')
